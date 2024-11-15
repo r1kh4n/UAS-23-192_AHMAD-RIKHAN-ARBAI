bcaaeaa37234ccc7e8bc23e2e5a02a4a7511a684
r1kh4n <143063624+r1kh4n@users.noreply.github.com>
2024-11-15 20:59:24 +0700

    Add files via upload

---
+++ db_user.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 15, 2024 at 02:58 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspirasi`
+--
+-- Error reading structure for table db_user.aspirasi: #1932 - Table 'db_user.aspirasi' doesn't exist in engine
+-- Error reading data for table db_user.aspirasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_user`.`aspirasi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentar`
+--
+-- Error reading structure for table db_user.komentar: #1932 - Table 'db_user.komentar' doesn't exist in engine
+-- Error reading data for table db_user.komentar: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_user`.`komentar`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `postingan`
+--
+-- Error reading structure for table db_user.postingan: #1932 - Table 'db_user.postingan' doesn't exist in engine
+-- Error reading data for table db_user.postingan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_user`.`postingan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_data`
+--
+-- Error reading structure for table db_user.user_data: #1932 - Table 'db_user.user_data' doesn't exist in engine
+-- Error reading data for table db_user.user_data: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_user`.`user_data`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wilayah`
+--
+-- Error reading structure for table db_user.wilayah: #1932 - Table 'db_user.wilayah' doesn't exist in engine
+-- Error reading data for table db_user.wilayah: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_user`.`wilayah`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
